68fec2329ee5c82dc975007e4a883ac94df3a3b4
Erik Landmark <erik@landmark.se>
2021-07-27 21:21:52 +0200

    deleted nodejs test files

--- nrf_radio_test.js
+++
@@ -1,8 +0,0 @@
-const cePin = 24;
-const irqPin = 25;
-const spiDev = "/dev/spidev0.0";
-
-const nrf = require('nrf');
-const radio = nrf.connect(spiDev, cePin, irqPin);
-radio._debug = true
-radio.printStatus()
