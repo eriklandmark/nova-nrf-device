b111f367b48b700a6f83735843ae2074fe408993
Erik Landmark <erik@landmark.se>
2021-08-15 02:17:52 +0200

    deleted symlinks for radio.ino

--- tools/setup.bat
+++ tools/setup.bat
@@ -1,6 +1,4 @@
 @ECHO OFF
 
-mklink /H arduino_sketches\nrf-device\radio.ino arduino_sketches\radio.ino
 mklink /H arduino_sketches\nrf-device\types.h arduino_sketches\types.h
-mklink /H arduino_sketches\nrf-gateway\radio.ino arduino_sketches\radio.ino
 mklink /H arduino_sketches\nrf-gateway\types.h arduino_sketches\types.h
