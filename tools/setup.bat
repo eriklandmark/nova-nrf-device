1cf1c551e9fdc2b57bd7d89ae4e1cbaabcd70d1e
Erik Landmark <erik@landmark.se>
2021-08-17 19:58:53 +0200

    added nrf-repeater

--- tools/setup.bat
+++ tools/setup.bat
@@ -2,3 +2,4 @@
 
 mklink /H arduino_sketches\nrf-device\types.h arduino_sketches\types.h
 mklink /H arduino_sketches\nrf-gateway\types.h arduino_sketches\types.h
+mklink /H arduino_sketches\nrf-repeater\types.h arduino_sketches\types.h
